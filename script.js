9154741cb22a194d847ade7652339cbb288014e7
Andrew102803 <106626454+Andrew102803@users.noreply.github.com>
2022-07-01 15:59:36 -0500

    Update script.js
    added my question varuiables, as well as the questions of the quiz and i mean thats basically all

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+})
+
+//questions and the correct answer
+
+var questions = [
+  {
+      questionTitle: "WhatsApp concurrent model is implemented using _____ programming language.",
+      choice1: "Java",
+      choice2: "Node.js",
+      choice3: "Erlang      ",
+      choice4: "C",
+      correct: "C"
+  },
+  {
+      questionTitle: "Which of the following is not a type of computer code related to Program Execution",
+      choice1: "Source code",
+      choice2: "Bytecode",
+      choice3: "Machine Code",
+      choice4: "Hex Code",
+      correct: "D"
+  },
+  {
+      questionTitle: "Which of the following is an apt description for an algorithm",
+      choice1: "A process or set of rules to be followed in calculations or other problem-solving operations, especially by a computer.",
+      choice2: "Program statements that run to execute a task.",
+      choice3: "A set of instructions written in a programming language to perform a task.",
+      choice4: "Set of instructions decoded from a high-level programming code to perform the task.",
+      correct: "A"
+  },
+  {
+      questionTitle: "Which of the following includes Chrome's V8 JavaScript Engine?",
+      choice1: "jQuery",
+      choice2: "java",
+      choice3: "npm",
+      choice4: "Node.js",
+      correct: "D"
+  },
+  {//this is a cry for help
+      questionTitle: "Python is",
+      choice1: "High level",
+      choice2: "dummy stuff",
+      choice3: "i am actually tired",
+      choice4: "why am i doing this",
+      correct: "A"
+  }
+]
